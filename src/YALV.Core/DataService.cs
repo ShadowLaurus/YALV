f7ef926509644f694d8a739cbff7d3a01330e992
Laurent DENIZOT <laurentbd52@gmail.com>
2017-01-27 14:48:44 +0100

    fix bug when loading log4j XML file who contains specials characters

--- src/YALV.Core/DataService.cs
+++ src/YALV.Core/DataService.cs
@@ -6,22 +6,16 @@ using System.Xml;
 using YALV.Core.Domain;
 using YALV.Core.Providers;
 
-namespace YALV.Core
-{
-    public static class DataService
-    {
-        public static void SaveFolderFile(IList<PathItem> folders, string path)
-        {
+namespace YALV.Core {
+    public static class DataService {
+        public static void SaveFolderFile(IList<PathItem> folders, string path) {
             FileStream fileStream = null;
             StreamWriter streamWriter = null;
-            try
-            {
-                if (folders != null)
-                {
+            try {
+                if (folders != null) {
                     fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
                     streamWriter = new StreamWriter(fileStream);
-                    foreach (PathItem item in folders)
-                    {
+                    foreach (PathItem item in folders) {
                         string line = String.Format("<folder name=\"{0}\" path=\"{1}\" />", item.Name, item.Path);
                         streamWriter.WriteLine(line);
                     }
@@ -30,28 +24,22 @@ namespace YALV.Core
                     fileStream.Close();
                     fileStream = null;
                 }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Trace.TraceError("Error saving Favorites list [{0}]:\r\n{1}\r\n{2}",path,ex.Message,ex.StackTrace);
+            } catch (Exception ex) {
+                System.Diagnostics.Trace.TraceError("Error saving Favorites list [{0}]:\r\n{1}\r\n{2}", path, ex.Message, ex.StackTrace);
                 throw;
-            }
-            finally
-            {
+            } finally {
                 if (streamWriter != null)
                     streamWriter.Close();
                 if (fileStream != null)
                     fileStream.Close();
             }
-           
+
         }
 
-        public static IList<PathItem> ParseFolderFile(string path)
-        {
+        public static IList<PathItem> ParseFolderFile(string path) {
             FileStream fileStream = null;
             StreamReader streamReader = null;
-            try
-            {
+            try {
                 FileInfo fileInfo = new FileInfo(path);
                 if (!fileInfo.Exists)
                     return null;
@@ -68,8 +56,7 @@ namespace YALV.Core
                 var xmlTextReader = new XmlTextReader(stringReader) { Namespaces = false };
 
                 IList<PathItem> result = new List<PathItem>();
-                while (xmlTextReader.Read())
-                {
+                while (xmlTextReader.Read()) {
                     if ((xmlTextReader.NodeType != XmlNodeType.Element) || (xmlTextReader.Name != "folder"))
                         continue;
 
@@ -77,14 +64,10 @@ namespace YALV.Core
                     result.Add(item);
                 }
                 return result;
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 System.Diagnostics.Trace.TraceError("Error parsing Favorites list [{0}]:\r\n{1}\r\n{2}", path, ex.Message, ex.StackTrace);
                 throw;
-            }
-            finally
-            {
+            } finally {
                 if (streamReader != null)
                     streamReader.Close();
                 if (fileStream != null)
@@ -92,20 +75,9 @@ namespace YALV.Core
             }
         }
 
-        public static IList<LogItem> ParseLogFile(string path)
-        {
-            IEnumerable<LogItem> result = null;
-            try
-            {
-                AbstractEntriesProvider provider = EntriesProviderFactory.GetProvider();
-                result = provider.GetEntries(path);
-                return result.ToList();
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Trace.TraceError("Error parsing log file [{0}]:\r\n{1}\r\n{2}", path, ex.Message, ex.StackTrace);
-                throw;
-            }
+        public static IEnumerable<LogItem> ParseLogFile(string path) {
+            foreach (var item in EntriesProviderFactory.GetProvider().GetEntries(path))
+                yield return item;
         }
     }
 }
--- src/YALV.Core/Providers/XmlEntriesProvider.cs
+++ src/YALV.Core/Providers/XmlEntriesProvider.cs
@@ -1,123 +1,133 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Xml;
+using System.Xml.Linq;
 using YALV.Core.Domain;
 
-namespace YALV.Core.Providers
-{
-    class XmlEntriesProvider : AbstractEntriesProvider
-    {
-        public override IEnumerable<LogItem> GetEntries(string dataSource, FilterParams filter)
-        {
-            var settings = new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Fragment };
-            var nt = new NameTable();
-            var mgr = new XmlNamespaceManager(nt);
-            mgr.AddNamespace("log4j", Constants.LAYOUT_LOG4J);
-            var pc = new XmlParserContext(nt, mgr, string.Empty, XmlSpace.Default);
-            var date = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+namespace YALV.Core.Providers {
+    class XmlEntriesProvider : AbstractEntriesProvider {
+        public override IEnumerable<LogItem> GetEntries(string dataSource, FilterParams filter) {
+            using (var stream = new FileStream(dataSource, FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read)) {
+                using (var reader = new StreamReader(stream, true)) {
+                    string lineFull = null;
+                    string line = null;
+                    while ((line = reader.ReadLine()) != null) {
+                        if (line.StartsWith("<log4j:event")) {
+                            lineFull = line;
+                        } else if (line.StartsWith("</log4j:event>")) {
+                            lineFull += line;
 
-            using (var stream = new FileStream(dataSource, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite))
-            {
-                using (var reader = new StreamReader(stream, System.Text.Encoding.Default, true))
-                {
-                    using (var xmlTextReader = XmlReader.Create(reader, settings, pc))
-                    {
-                        var entryId = 1;
-                        DateTime? prevTimeStamp = null;
-                        while (xmlTextReader.Read())
-                        {
-                            if ((xmlTextReader.NodeType != XmlNodeType.Element) || (xmlTextReader.Name != "log4j:event"))
-                                continue;
+                            //yield return GetEntry(dataSource, new StringReader(lineFull));
+                            yield return GetEntries(dataSource, new StringReader(lineFull), filter).FirstOrDefault();
 
-                            var entry = new LogItem { Id = entryId, Path = dataSource };
-
-                            entry.Logger = xmlTextReader.GetAttribute("logger");
-
-                            entry.TimeStamp = date.AddMilliseconds(Convert.ToDouble(xmlTextReader.GetAttribute("timestamp"))).ToLocalTime();
-                            if (prevTimeStamp.HasValue)
-                                entry.Delta = (entry.TimeStamp - prevTimeStamp.Value).TotalSeconds;
-                            prevTimeStamp = entry.TimeStamp;
-
-                            entry.Level = xmlTextReader.GetAttribute("level");
-                            entry.Thread = xmlTextReader.GetAttribute("thread");
-
-                            while (xmlTextReader.Read())
-                            {
-                                var breakLoop = false;
-                                switch (xmlTextReader.Name)
-                                {
-                                    case "log4j:event":
-                                        breakLoop = true;
-                                        break;
-                                    default:
-                                        switch (xmlTextReader.Name)
-                                        {
-                                            case ("log4j:message"):
-                                                entry.Message = xmlTextReader.ReadString();
-                                                break;
-                                            case ("log4j:data"):
-                                                switch (xmlTextReader.GetAttribute("name"))
-                                                {
-                                                    case ("log4net:UserName"):
-                                                        entry.UserName = xmlTextReader.GetAttribute("value");
-                                                        break;
-                                                    case ("log4japp"):
-                                                        entry.App = xmlTextReader.GetAttribute("value");
-                                                        break;
-                                                    case ("log4jmachinename"):
-                                                        entry.MachineName = xmlTextReader.GetAttribute("value");
-                                                        break;
-                                                    case ("log4net:HostName"):
-                                                        entry.HostName = xmlTextReader.GetAttribute("value");
-                                                        break;
-													default:
-		                                                var name = xmlTextReader.GetAttribute("name");
-		                                                if (!name.StartsWith("log4net:"))
-		                                                {
-															var val = xmlTextReader.GetAttribute("value");
-															entry.CustomFields.Add(name, val);
-														}
-														break;
-                                                }
-                                                break;
-                                            case ("log4j:throwable"):
-                                                entry.Throwable = xmlTextReader.ReadString();
-                                                break;
-                                            case ("log4j:locationInfo"):
-                                                entry.Class = xmlTextReader.GetAttribute("class");
-                                                entry.Method = xmlTextReader.GetAttribute("method");
-                                                entry.File = xmlTextReader.GetAttribute("file");
-                                                entry.Line = xmlTextReader.GetAttribute("line");
-                                                break;
-                                        }
-                                        break;
-                                }
-                                if (breakLoop) break;
-                            }
-
-                            if (filterByParameters(entry, filter))
-                            {
-                                yield return entry;
-                                entryId++;
-                            }
+                            lineFull = null;
+                        } else {
+                            lineFull += line;
                         }
                     }
                 }
             }
-
         }
 
-        private static bool filterByParameters(LogItem entry, FilterParams parameters)
-        {
+        private IEnumerable<LogItem> GetEntries(string path, TextReader reader, FilterParams filter) {
+            var date = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            var nt = new NameTable();
+            var mgr = new XmlNamespaceManager(nt);
+            var pc = new XmlParserContext(nt, mgr, string.Empty, XmlSpace.Default);
+            var settings = new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Fragment };
+            mgr.AddNamespace("log4j", Constants.LAYOUT_LOG4J);
+
+            using (var xmlTextReader = XmlReader.Create(reader, settings, pc)) {
+                var entryId = 1;
+                DateTime? prevTimeStamp = null;
+                while (xmlTextReader.Read()) {
+                    if ((xmlTextReader.NodeType != XmlNodeType.Element) || (xmlTextReader.Name != "log4j:event"))
+                        continue;
+
+                    LogItem entry = new LogItem {
+                        Id = entryId,
+                        Path = path,
+                        Level = xmlTextReader.GetAttribute("level"),
+                        Thread = xmlTextReader.GetAttribute("thread"),
+                        Logger = xmlTextReader.GetAttribute("logger")
+                    };
+
+                    entry.TimeStamp = date.AddMilliseconds(Convert.ToDouble(xmlTextReader.GetAttribute("timestamp"))).ToLocalTime();
+                    if (prevTimeStamp.HasValue)
+                        entry.Delta = (entry.TimeStamp - prevTimeStamp.Value).TotalSeconds;
+                    prevTimeStamp = entry.TimeStamp;
+
+                    try {
+                        while (xmlTextReader.Read()) {
+                            if (xmlTextReader.NodeType == XmlNodeType.EndElement && xmlTextReader.Name == "log4j:event")
+                                break;
+
+                            switch (xmlTextReader.Name) {
+                                case ("log4j:message"):
+                                    try {
+                                        entry.Message = xmlTextReader.ReadString();
+                                    } catch (Exception ex) {
+                                        entry.Message = "<Error reading message: " + ex.Message + ">";
+                                    }
+                                    break;
+                                case ("log4j:data"):
+                                    switch (xmlTextReader.GetAttribute("name")) {
+                                        case ("log4net:UserName"):
+                                            entry.UserName = xmlTextReader.GetAttribute("value");
+                                            break;
+                                        case ("log4japp"):
+                                            entry.App = xmlTextReader.GetAttribute("value");
+                                            break;
+                                        case ("log4jmachinename"):
+                                            entry.MachineName = xmlTextReader.GetAttribute("value");
+                                            break;
+                                        case ("log4net:HostName"):
+                                            entry.HostName = xmlTextReader.GetAttribute("value");
+                                            break;
+                                        default:
+                                            var name = xmlTextReader.GetAttribute("name");
+                                            if (!name.StartsWith("log4net:")) {
+                                                var val = xmlTextReader.GetAttribute("value");
+                                                entry.CustomFields.Add(name, val);
+                                            }
+                                            break;
+                                    }
+                                    break;
+                                case ("log4j:throwable"):
+                                    entry.Throwable = xmlTextReader.ReadString();
+                                    break;
+                                case ("log4j:locationInfo"):
+                                    entry.Class = xmlTextReader.GetAttribute("class");
+                                    entry.Method = xmlTextReader.GetAttribute("method");
+                                    entry.File = xmlTextReader.GetAttribute("file");
+                                    entry.Line = xmlTextReader.GetAttribute("line");
+                                    break;
+                            }
+                            break;
+                        }
+                    } catch (Exception ex) {
+                        entry.Message = "<Error reading message: " + ex.Message + ">";
+                    }
+
+                    if (filterByParameters(entry, filter)) {
+                        yield return entry;
+                        entryId++;
+                    }
+                }
+            }
+        }
+
+        private static bool filterByParameters(LogItem entry, FilterParams parameters) {
             if (entry == null)
                 throw new ArgumentNullException("entry");
             if (parameters == null)
                 throw new ArgumentNullException("parameters");
 
             bool accept = false;
-            switch (parameters.Level)
-            {
+            switch (parameters.Level) {
                 case 1:
                     if (String.Equals(entry.Level, "ERROR",
                         StringComparison.InvariantCultureIgnoreCase))
